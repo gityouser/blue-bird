8d8a5b742d5fbb1f9f2e4b3db5ab48aa9ad62d7c
Andrei Constantinescu <andreiconstantinescumail@gmail.com>
2023-08-02 17:27:14 +0300

    hook up supabase signin and redirect

--- app/auth-button.tsx
+++ app/auth-button.tsx
@@ -1,8 +1,16 @@
 "use client";
 
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+
 export function AuthButton() {
+  const supabase = createClientComponentClient();
   const handleSignIn = async () => {
-    console.log("clicked :>> ");
+    await supabase.auth.signInWithOAuth({
+      provider: "github",
+      options: {
+        redirectTo: "http://localhost:3000/auth/callback",
+      },
+    });
   };
   return <button onClick={handleSignIn}>Login</button>;
 }
