70fd3b8fbd9aa5d07c06798c7c45788221b887f4
Andrei Constantinescu <andreiconstantinescumail@gmail.com>
2023-08-05 21:35:22 +0300

    refresh route on logout to refetch tweets for the unauthenticated user

--- app/auth-button.tsx
+++ app/auth-button.tsx
@@ -1,9 +1,11 @@
 "use client";
 
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { useRouter } from "next/navigation";
 
 export function AuthButton() {
   const supabase = createClientComponentClient();
+  const router = useRouter();
   const handleSignIn = async () => {
     await supabase.auth.signInWithOAuth({
       provider: "github",
@@ -15,6 +17,7 @@ export function AuthButton() {
 
   const handleSignOut = async () => {
     await supabase.auth.signOut();
+    router.refresh();
   };
 
   return (
