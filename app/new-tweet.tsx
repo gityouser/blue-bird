a0fa2f2e032641e9fb089703f6e81d88fe46f7a8
Andrei Constantinescu <andreiconstantinescumail@gmail.com>
2023-08-10 22:01:09 +0300

    add new tweet

--- app/new-tweet.tsx
+++ app/new-tweet.tsx
@@ -1,7 +1,16 @@
+import { createServerActionClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+
 export default function NewTweet() {
-  const addTweet = async () => {
+  const addTweet = async (formData: FormData) => {
     "use server";
-    console.log("submitted");
+    const title = String(formData.get("title"));
+    const supabase = createServerActionClient<Database>({ cookies });
+    const {
+      data: { user },
+    } = await supabase.auth.getUser();
+
+    if (user) await supabase.from("tweets").insert({ title, user_id: user.id });
   };
 
   return (
