69d9e8400638c0c1cf1faf1800168690a23981b4
Andrei Constantinescu <andreiconstantinescumail@gmail.com>
2023-08-17 22:47:12 +0300

    add heart like button

--- app/likes.tsx
+++ app/likes.tsx
@@ -42,5 +42,27 @@ export default function Likes({
     }
   };
 
-  return <button onClick={handleLikes}>{tweet.likes} Likes</button>;
+  return (
+    <button onClick={handleLikes}>
+      {tweet.likes}
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        className={`${
+          tweet.isLiked
+            ? "fill-red-600 stroke-red-600"
+            : "fill-none stroke-gray-500"
+        }`}
+      >
+        <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path>
+      </svg>{" "}
+      Likes
+    </button>
+  );
 }
