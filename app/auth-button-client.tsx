21937feadd62583dacd9f22444c37836d8324dd5
Andrei Constantinescu <andreiconstantinescumail@gmail.com>
2023-08-18 15:15:20 +0300

    point links to window.location

--- app/auth-button-client.tsx
+++ app/auth-button-client.tsx
@@ -14,7 +14,7 @@ export function AuthButtonClient({ session }: { session: Session | null }) {
     await supabase.auth.signInWithOAuth({
       provider: "github",
       options: {
-        redirectTo: "http://localhost:3000/auth/callback",
+        redirectTo: `${window.location}/auth/callback`,
       },
     });
   };
--- app/login/github-button.tsx
+++ app/login/github-button.tsx
@@ -10,7 +10,7 @@ export default function GithubButton() {
     await supabase.auth.signInWithOAuth({
       provider: "github",
       options: {
-        redirectTo: "http://localhost:3000/auth/callback",
+        redirectTo: `${window.location}/auth/callback`,
       },
     });
   };
