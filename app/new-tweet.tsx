bc518a4baf73265237ece38c3fe3ffa3d37a22e8
Andrei Constantinescu <andreiconstantinescumail@gmail.com>
2023-08-22 00:08:15 +0300

    add input border and focus

--- app/new-tweet.tsx
+++ app/new-tweet.tsx
@@ -17,7 +17,7 @@ export default function NewTweet({ user }: { user: User }) {
     <form className="border border-gray-800 border-t-0" action={addTweet}>
       <div className="flex py-8 px-4">
         <Image
-          src={user.user_metadata.avatar_url}
+          src={user?.user_metadata.avatar_url}
           width={48}
           height={48}
           className="bg-red-200 h-12 w-12 rounded-full"
@@ -25,7 +25,7 @@ export default function NewTweet({ user }: { user: User }) {
         />
         <input
           name="title"
-          className="bg-inherit flex-1 ml-2 text-2xl leading-loose placeholder-gray-500 px-2"
+          className="bg-inherit flex-1 ml-2 lg:text-2xl leading-loose placeholder-gray-500 px-2 rounded-md border-0 ring-1 ring-inset ring-gray-800 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
           placeholder="What is happening?!"
         />
       </div>
